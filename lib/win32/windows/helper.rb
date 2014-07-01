7a71a5cf7c3265754822f5c0de34cc45899eb56b
Ethan J. Brown <Iristyle@github>
2014-07-01 15:45:41 -0700

    Define constants used by FormatMessage

--- lib/win32/windows/helper.rb
+++ lib/win32/windows/helper.rb
@@ -9,9 +9,13 @@ module Windows
     attach_function :FormatMessage, :FormatMessageA,
     [:uint32, :pointer, :uint32, :uint32, :pointer, :uint32, :pointer], :uint32
 
+    FORMAT_MESSAGE_IGNORE_INSERTS    = 0x00000200
+    FORMAT_MESSAGE_FROM_SYSTEM       = 0x00001000
+
     def win_error(function, err=FFI.errno)
       error_msg = ''
-      flags = 0x00001000 | 0x00000200
+      flags = FORMAT_MESSAGE_FROM_SYSTEM |
+              FORMAT_MESSAGE_IGNORE_INSERTS
       FFI::MemoryPointer.new(:char, 1024) do |buf|
 
         FormatMessage(flags, nil, err , 0x0409, buf, 1024, nil)
