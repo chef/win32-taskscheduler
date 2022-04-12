c9be2ecd7bb844d6d8c2ca61704245c39a6abbbf
antima-gupta <agupta@msystechnologies.com>
2022-04-12 18:38:25 +0530

    Fixed the style failure.
    Signed-off-by: antima-gupta <agupta@msystechnologies.com>

--- lib/win32/taskscheduler/time_calc_helper.rb
+++ lib/win32/taskscheduler/time_calc_helper.rb
@@ -127,12 +127,11 @@ module Win32
       #   time_details("PT3S") #=> {sec: 3}
       #
       def time_details(time_str)
-
         tm_detail = {}
         if time_str.to_s != ""
           # raising exception if time is not a string
           raise TypeError.new("TypeError: Inccorrect Type please pass input value as a String.") unless time_str.is_a? String
-         
+
           # time_str will be like "PxxYxxMxxDTxxHxxMxxS"
           # Ignoring 'P' and extracting date and time
           dt, tm = time_str[1..-1].split("T")
