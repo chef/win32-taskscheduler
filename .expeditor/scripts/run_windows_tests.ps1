24fe84e22c7b0d89e3f2bbb0e4a0d989d3aa0a2f
rishichawda <rishichawda@users.noreply.github.com>
2025-06-10 18:44:58 +0530

    install ruby
    Signed-off-by: rishichawda <rishichawda@users.noreply.github.com>

--- .expeditor/scripts/run_windows_tests.ps1
+++ .expeditor/scripts/run_windows_tests.ps1
@@ -1,22 +1,31 @@
 # Stop script execution when a non-terminating error occurs
 $ErrorActionPreference = "Stop"
 
-# This will run ruby test on windows platform
+# Find the latest Ruby install directory
+$RubyDir = Get-ChildItem -Directory "C:\Ruby*" | Sort-Object LastWriteTime -Descending | Select-Object -First 1
+if ($RubyDir) {
+    $RubyBin = "$($RubyDir.FullName)\bin"
+    Write-Output "Ruby installed at $RubyBin"
+} else {
+    throw "Could not find Ruby installation directory."
+}
+$env:PATH = "$env:PATH;$RubyBin"
+
+Write-Output "--- Installed Ruby version"
+ruby --version
 
 Write-Output "--- Bundle install"
 
 bundle config --local path vendor/bundle
-If ($lastexitcode -ne 0) { Exit $lastexitcode }
-
 bundle install --jobs=7 --retry=3
-If ($lastexitcode -ne 0) { Exit $lastexitcode }
+if (-not $?) { throw "bundle install failed" }
 
 Write-Output "--- Running Cookstyle"
 gem install cookstyle
 cookstyle --chefstyle -c .rubocop.yml
-If ($lastexitcode -ne 0) { Exit $lastexitcode }
+if (-not $?) { throw "Cookstyle failed." }
 
 Write-Output "--- Bundle Execute"
 
 bundle exec rake
-If ($lastexitcode -ne 0) { Exit $lastexitcode }
+if (-not $?) { throw "Rake task failed." }
